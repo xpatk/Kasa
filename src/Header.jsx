c259f19ac1c9fd782130d5ca4893a7f0328ef898
xpat <patrycja.kruczynska@gmail.com>
2024-07-12 10:55:43 +0200

    initialization

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <img></img>
+      <ul>
+        <li>Accueil</li>
+        <li>A propos</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
