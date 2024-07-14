eddd125e81ce4ceb76ddf61fd55ac75f72cb72d6
xpat <patrycja.kruczynska@gmail.com>
2024-07-14 17:17:04 +0200

    changed folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
