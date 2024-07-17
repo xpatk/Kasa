109cf1ceae7846822d57b9e42f900bed1597f2ee
xpat <patrycja.kruczynska@gmail.com>
2024-07-18 00:45:36 +0200

    added footer style

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/index.scss";
+import "./style/main.scss";
 import {
   createBrowserRouter,
   createRoutesFromElements,
