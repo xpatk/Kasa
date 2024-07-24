57ad00c2726119a6a44c7cb1b75d7bc025710f64
xpat <patrycja.kruczynska@gmail.com>
2024-07-24 19:06:56 +0200

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,14 +5,30 @@ function Slider({ pictures }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentImage = pictures[currentIndex];
 
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? pictures.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === pictures.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <div className="container-slider">
       <div
         className="img-slider"
         style={{ backgroundImage: `url(${currentImage})` }}
       >
-        <div className="left-arrow">&#10094;</div>
-        <div className="right-arrow">&#10095;</div>
+        <div className="left-arrow" onClick={goToPrevious}>
+          &#10094;
+        </div>
+        <div className="right-arrow" onClick={goToNext}>
+          &#10095;
+        </div>
       </div>
     </div>
   );
