6bd5cf081f6a7589e8f25b92aba01bb656d50823
xpat <patrycja.kruczynska@gmail.com>
2024-07-30 20:43:09 +0200

    finished 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Page404() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Ooups. La page n&apos;existe pas.</p>
+    <div className="page404-container">
+      <h1 className="page404-header">404</h1>
+      <p className="page404-par">
+        Ooups. La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to="">
+        <p className="page404-link">Retourner sur la page d&apos;accueil.</p>
+      </Link>
     </div>
   );
 }
